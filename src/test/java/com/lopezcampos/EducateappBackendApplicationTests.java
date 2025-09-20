dce5bd62433289c75722ef7d17fd0ad91c89e87c
Erick Lopez <ecklpz@gmail.com>
2025-09-20 14:11:43 -0500

    chore: initial structure

---
+++ src/test/java/com/lopezcampos/EducateappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopezcampos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducateappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
